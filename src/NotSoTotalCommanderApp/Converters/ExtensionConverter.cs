8e639c990d53f14f18e810459d6d0a18da355566
Arasz <rafal.araszkiewicz@student.put.poznan.pl>
2016-04-24 21:05:18 +0200

    Add Converters

---
+++ src/NotSoTotalCommanderApp/Converters/ExtensionConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace NotSoTotalCommanderApp.Converters
+{
+    /// <summary>
+    /// Formats extension string 
+    /// </summary>
+    internal class ExtensionConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var extension = value as string;
+            return extension?.TrimStart('.');
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/NotSoTotalCommanderApp/Converters/SizeConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace NotSoTotalCommanderApp.Converters
+{
+    /// <summary>
+    /// Formats size string received from FileSystemInfo 
+    /// </summary>
+    internal class SizeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return "<DIR>";
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
