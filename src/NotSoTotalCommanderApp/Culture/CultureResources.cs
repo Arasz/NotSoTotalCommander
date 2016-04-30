a1cb8eaf5717d3dcf75663a5bf4a072d3b4ceee0
Arasz <rafal.araszkiewicz@student.put.poznan.pl>
2016-05-01 00:55:47 +0200

    Add Log4Net and update mvvm light

---
+++ src/NotSoTotalCommanderApp/Culture/CultureResources.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace NotSoTotalCommanderApp.Culture
+{
+    public class CultureResources
+    {
+        private static readonly ObjectDataProvider ResourceProvider =
+            (ObjectDataProvider)Application.Current.FindResource("Resources");
+
+        public static void ChangeCulture(CultureInfo culture)
+        {
+            Properties.Resources.Culture = culture;
+            ResourceProvider.Refresh();
+        }
+
+        public Properties.Resources GetResourceInstance() => new Properties.Resources();
+    }
+}
---
+++ src/NotSoTotalCommanderApp/Utility/IocContainer.cs
@@ -0,0 +1,23 @@
+﻿using GalaSoft.MvvmLight.Ioc;
+
+namespace NotSoTotalCommanderApp.Utility
+{
+    /// <summary>
+    /// Registers all types other than view models inside IoC 
+    /// </summary>
+    public class IocContainer
+    {
+        private SimpleIoc _container = new SimpleIoc();
+
+        public SimpleIoc DefualtContainer => _container;
+
+        public IocContainer()
+        {
+            Configure();
+        }
+
+        private void Configure()
+        {
+        }
+    }
+}
