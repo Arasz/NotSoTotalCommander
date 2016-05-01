8ec918900e9532520447660681fd914694904eba
Arasz <rafal.araszkiewicz@student.put.poznan.pl>
2016-05-01 12:20:14 +0200

    Add new exception

---
+++ src/NotSoTotalCommanderApp/Exceptions/MoveOperationException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NotSoTotalCommanderApp.Exceptions
+{
+    internal class MoveOperationException : FileSystemException
+    {
+        public string DestinationPath { get; }
+
+        public MoveOperationException(string sourcePath, string destinationPath, string message = "", Exception innerException = null) : base(sourcePath, message, innerException)
+        {
+            DestinationPath = destinationPath;
+        }
+    }
+}
