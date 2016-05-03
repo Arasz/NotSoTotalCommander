6dccf90a04e7e8f15dbdebf307999db2f3a28605
Arasz <rafal.araszkiewicz@student.put.poznan.pl>
2016-05-03 16:16:03 +0200

    Add resources translation and fix date format

---
+++ src/NotSoTotalCommanderApp/Culture/StaticDateTimeFormat.cs
@@ -0,0 +1,7 @@
+﻿namespace NotSoTotalCommanderApp.Culture
+{
+    internal static class StaticDateTimeFormat
+    {
+        public static string ShortDate => Properties.Resources.Culture.DateTimeFormat.ShortDatePattern;
+    }
+}
